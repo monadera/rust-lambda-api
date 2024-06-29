e4284b6ea591f92259aacecf6430ac9564b14362
David Steiner <david_j_steiner@yahoo.co.nz>
2024-06-29 17:37:41 -0400

    Add main function for local development

--- src/bin/local.rs
+++ src/bin/local.rs
@@ -1,6 +1,18 @@
 use anyhow::Result;
+use poem::listener::TcpListener;
+use std::sync::Arc;
+
+use serverless_rust_api::api::build_app;
+use serverless_rust_api::repository::InMemoryRepository;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt().pretty().init();
+    let repository = Arc::new(InMemoryRepository::default());
+    let app = build_app(repository)?;
+    poem::Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await?;
+
     Ok(())
 }
